8ac1efae850c7422b193aae746210d0bbffd48be
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-06-03 21:59:07 -0300

    Personalizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import {
   useFonts,
   Roboto_400Regular,
@@ -12,7 +12,19 @@ export default function App() {
   });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Hello World</Text> : <View />}
     </View>
   );
