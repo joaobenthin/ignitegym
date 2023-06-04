e8991420913cb7e991e8e6b86a6a2908aec4ae71
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-06-03 22:32:25 -0300

    Utilizando NativeBase

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
+import { Loading } from '@components/Loading';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
@@ -19,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <View /> : <View />}
+      {!fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
