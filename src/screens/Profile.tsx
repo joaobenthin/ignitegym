80d20d203b45145e451870757421444582395b56
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-09-01 22:54:57 -0300

    Inputs de senha

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from 'native-base';
 
-import { ScreenHeader } from '@components/ScreenHeader';
-import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+import { UserPhoto } from '@components/UserPhoto';
+import { ScreenHeader } from '@components/ScreenHeader';
 
 const PHOTO_SIZE = 33;
 
@@ -47,6 +55,22 @@ export function Profile() {
           <Input bg="gray.600" placeholder="Nome" />
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
