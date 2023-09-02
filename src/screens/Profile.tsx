830fa0b921bf0537dc88f1d090bf8ba95ea8c162
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-09-01 23:03:09 -0300

    Recap da Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -22,7 +22,7 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -54,10 +54,14 @@ export function Profile() {
 
           <Input bg="gray.600" placeholder="Nome" />
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
-        </Center>
 
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
             Alterar senha
           </Heading>
 
@@ -70,7 +74,7 @@ export function Profile() {
           />
 
           <Button title="Atualizar" mt={4} />
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   );
