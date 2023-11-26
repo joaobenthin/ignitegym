b57c8d39d3d8b6f9f9c5206c00160f35782ca506
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-11-26 20:32:37 -0300

    Async Await

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -44,15 +44,18 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password }: FormDataProps) {
-    fetch('http://127.0.0.1:3333/users', {
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    const response = await fetch('http://127.0.0.1:3333/users', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ name, email, password }),
-    }).then(response => response.json().then(data => console.log(data)));
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
