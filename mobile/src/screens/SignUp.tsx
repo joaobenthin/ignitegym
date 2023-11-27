ee61ffc4b82917bdd4f62e6a8370421332729aba
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-11-26 21:09:01 -0300

    Entendendo Interceptors

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -12,6 +12,7 @@ import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { Alert } from 'react-native';
 
 type FormDataProps = {
   name: string;
@@ -52,7 +53,7 @@ export function SignUp() {
       const response = await api.post('/users', { name, email, password });
       console.log(response.data);
     } catch (error) {
-      if (axios.isAxiosError(error)) console.log(error.response?.data.message);
+      if (axios.isAxiosError(error)) Alert.alert(error.response?.data.message);
     }
   }
 
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://127.0.0.1:3333',
 });
+
+api.interceptors.response.use(
+  response => {
+    console.log('INTERCEPTOR RESPONSE =>', response);
+
+    return response;
+  },
+  error => {
+    console.log('INTERCEPTOR RESPONSE =>', error);
+    return Promise.reject(error);
+  },
+);
+
+export { api };
